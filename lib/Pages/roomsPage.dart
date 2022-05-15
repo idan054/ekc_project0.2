fcf2c56b61bffb6fbd7bd92fb808ba3cc0303354
Idan054 <idanbit80@gmail.com>
2022-05-15 15:04:31 +0300

    RiTopia WOW V1.2.0 - Icon & unread badge Ui [ ] Make unread badge work [ ] Allow moderator delete by hold (user TBH will become moderator manually from firebase] [ ] Create beautiful
    RilTopia WOW MVP: V1.0.4 [GPlay] - Change package to com.biton.riltopiaFlyer and sha1 google-services.json
    
    [X] Fixed DM msgs ui chat
    
    [X] A_loginPage.dart in RilTopia design
    [X] age (and full user details) added to every message metadata!
    [X] Remove chat with yourself button
    
    Gang X6 - Cant believe but i did it!
    + Cards show user pic + name bug FIXED
    
    Journey diary: Look likes i just fixed the last bug for today?? in the last thursday ive made a challenge To Finish. The MVP. I cant belive i might did it! congrats! 🎉
    
    [X] Boarding explain (1)
    [X] Google login (2)
    [X] Remember logged user (2.1) - Fiverr Dev (?)
    [X] profilePage.dart to select ur birthday, only (3)
    [X] Main chat (4)
    [X] Main chat ui (4.1) - Cards
    [X] Main chat ui (4.2) - Riltopia BottomNavigator
    [X] cooldown Main chat (5) DONE BETA: Snackbar
    [X] age filter Main chat (6)
    [X] chatList (7)
    [X] private chat (8)
    Done! launch to stores!

--- lib/Pages/roomsPage.dart
+++ lib/Pages/roomsPage.dart
@@ -5,6 +5,7 @@ import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:flutter_firebase_chat_core/flutter_firebase_chat_core.dart';
 
 import '../Widgets/myAppBar.dart';
+import '../theme/colors.dart';
 import '../theme/constants.dart';
 import 'A_loginPage.dart';
 import 'flyerDm.dart';
@@ -58,138 +59,152 @@ class RoomsPage extends StatelessWidget {
                       const SizedBox(height: 10),
                       Directionality(
                         textDirection: TextDirection.rtl,
-                        child: Card(
-                          margin: const EdgeInsets.symmetric(
-                              horizontal: 8.0, vertical: 2.0),
-                          shape: RoundedRectangleBorder(
-                            side: BorderSide(
-                                color: Colors.grey[200]!, width: 1.5),
-                            borderRadius: BorderRadius.circular(6.0),
-                          ),
-                          elevation: 0,
-                          shadowColor: Colors.black87,
-                          color: Colors.grey[100]!,
-                          child: Column(
-                            children: [
-                              const SizedBox(
-                                height: 2,
-                              ),
-                              /*Container(
-                              padding: const EdgeInsets.only(right: 10, left: 10),
-                              alignment: Alignment.centerRight,
-                              child:
-                              InkWell(
+                        child: InkWell(
+                          splashColor: Colors.black26,
+                          onTap: () async {
+                            final room = await FirebaseChatCore.instance.createRoom(otherUser!);
+                            kPushNavigator(context, FlyerDm(room: room,));
+                          },
+                          child: Card(
+                            margin: const EdgeInsets.symmetric(
+                                horizontal: 8.0, vertical: 2.0),
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(
+                                  color: Colors.grey[200]!, width: 1.5),
+                              borderRadius: BorderRadius.circular(6.0),
+                            ),
+                            elevation: 0,
+                            shadowColor: Colors.black87,
+                            color: Colors.grey[100]!,
+                            child: Column(
+                              children: [
+                                const SizedBox(
+                                  height: 2,
+                                ),
+                                /*Container(
+                                padding: const EdgeInsets.only(right: 10, left: 10),
+                                alignment: Alignment.centerRight,
                                 child:
-                                Icon(
-                                  Icons.more_horiz,
-                                  color: Colors.grey[200]!,
+                                InkWell(
+                                  child:
+                                  Icon(
+                                    Icons.more_horiz,
+                                    color: Colors.grey[200]!,
+                                  ),
+                                  onTap:() {},
                                 ),
-                                onTap:() {},
-                              ),
-                            ),*/
+                              ),*/
 /*                            Container(
-                              padding: const EdgeInsets.only(right: 10, left: 10),
-                              alignment: Alignment.topRight,
-                              child: Text(text,
-                                style: const TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.normal,
-                                  fontSize: 16,
+                                padding: const EdgeInsets.only(right: 10, left: 10),
+                                alignment: Alignment.topRight,
+                                child: Text(text,
+                                  style: const TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.normal,
+                                    fontSize: 16,
+                                  ),
                                 ),
-                              ),
-                            ),*/
-                              Container(
-                                height: 80,
-                                padding: const EdgeInsets.only(right: 10),
-                                // color: Colors.primaries[Random().nextInt(Colors.primaries.length)].shade300,
-                                // color: cGrey100,
-                                child: Row(
-                                  children: [
-                                    Flexible(
-                                      child: ListTile(
-                                        dense: true,
-                                        visualDensity: VisualDensity.standard,
-                                        title: Text(
-                                          '${otherUser?.firstName}',
-                                          style: TextStyle(
-                                              // color: Colors.primaries[Random().nextInt(Colors.primaries.length)].shade600,
-                                              // color: Colors.black
-                                              color: Colors.grey[600]!,
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 14),
-                                          // style: bodyText1Format(context)
-                                        ),
-                                        subtitle: Text(
-                                          /*' · '*/
-                                          '(${otherUser?.metadata?['age'].toString().substring(0, 2)})',
-                                          textDirection: TextDirection.rtl,
-                                          style: TextStyle(
-                                              // color: Colors.primaries[Random().nextInt(Colors.primaries.length)].shade600,
-                                              // color: Colors.black
-                                              color: Colors.grey[600],
-                                              fontWeight: FontWeight.normal,
-                                              fontSize: 12),
-                                          // style: bodyText1Format(context)
-                                        ),
-                                        contentPadding: EdgeInsets.zero,
-                                        leading: /* CircleAvatar(
-                                          backgroundImage: NetworkImage('${otherUser?.imageUrl}'),
-                                          // backgroundImage: NetworkImage('https://bit.ly/3l64LIk'),
-                                        )*/
-                                            CircleAvatar(
-                                                backgroundColor:
-                                                    Colors.grey[400]!,
-                                                radius: 44 / 2,
-                                                child: CircleAvatar(
+                              ),*/
+                                Container(
+                                  height: 80,
+                                  padding: const EdgeInsets.only(right: 10),
+                                  // color: Colors.primaries[Random().nextInt(Colors.primaries.length)].shade300,
+                                  // color: cGrey100,
+                                  child: Row(
+                                    children: [
+                                      Flexible(
+                                        child: ListTile(
+                                          dense: true,
+                                          visualDensity: VisualDensity.standard,
+                                          title: Text(
+                                            '${otherUser?.firstName}',
+                                            style: TextStyle(
+                                                // color: Colors.primaries[Random().nextInt(Colors.primaries.length)].shade600,
+                                                // color: Colors.black
+                                                color: Colors.grey[600]!,
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 16),
+                                            // style: bodyText1Format(context)
+                                          ),
+                                          subtitle: Row(
+                                            children: [
+                                              Text(
+                                                /*' · '*/
+                                                '(${otherUser?.metadata?['age'].toString().substring(0, 2)})',
+                                                textDirection: TextDirection.rtl,
+                                                style: TextStyle(
+                                                    // color: Colors.primaries[Random().nextInt(Colors.primaries.length)].shade600,
+                                                    // color: Colors.black
+                                                    color: Colors.grey[600],
+                                                    fontWeight: FontWeight.normal,
+                                                    fontSize: 14),
+                                                // style: bodyText1Format(context)
+                                              ),
+                                              const SizedBox(width: 5,),
+                                              const CircleAvatar(
+                                                radius: 10,
+                                                backgroundColor: Colors.red,
+                                                child: Text('2',
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                      // color: Colors.grey[600],
+                                                      fontWeight: FontWeight.bold,
+                                                      fontSize: 12)
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                          contentPadding: EdgeInsets.zero,
+                                          leading: /* CircleAvatar(
+                                            backgroundImage: NetworkImage('${otherUser?.imageUrl}'),
+                                            // backgroundImage: NetworkImage('https://bit.ly/3l64LIk'),
+                                          )*/
+                                              CircleAvatar(
                                                   backgroundColor:
                                                       Colors.grey[400]!,
-                                                  radius: 39 / 2,
-                                                  backgroundImage: NetworkImage(
-                                                      '${otherUser?.imageUrl}'),
-                                                )),
+                                                  radius: 44 / 2,
+                                                  child: CircleAvatar(
+                                                    backgroundColor:
+                                                        Colors.grey[400]!,
+                                                    radius: 39 / 2,
+                                                    backgroundImage: NetworkImage(
+                                                        '${otherUser?.imageUrl}'),
+                                                  )),
+                                        ),
                                       ),
-                                    ),
 
-                                    Builder(
-                                        builder: (context) => Padding(
-                                              padding: const EdgeInsets.only(
-                                                  left: 10),
-                                              child: Directionality(
-                                                textDirection:
-                                                    TextDirection.rtl,
-                                                child: CircleAvatar(
-                                                  backgroundColor:
-                                                      Colors.grey[200],
-                                                  radius: 20,
-                                                  child: IconButton(
-                                                      onPressed: () async {
-                                                        final room =
-                                                            await FirebaseChatCore
-                                                                .instance
-                                                                .createRoom(
-                                                                    otherUser!);
-
-                                                        kPushNavigator(
-                                                            context,
-                                                            FlyerDm(
-                                                              room: room,
-                                                            ));
-                                                      },
-                                                      icon: Icon(
-                                                        Icons.send_rounded,
-                                                        color: Colors.grey[500],
-                                                        size: 20,
-                                                      )),
+                                      Builder(
+                                          builder: (context) => Padding(
+                                                padding: const EdgeInsets.only(
+                                                    left: 10),
+                                                child: Directionality(
+                                                  textDirection:
+                                                      TextDirection.rtl,
+                                                  child: CircleAvatar(
+                                                    backgroundColor:
+                                                        Colors.grey[200],
+                                                    radius: 20,
+                                                    child: IconButton(
+                                                        onPressed: () async {
+                                                          final room = await FirebaseChatCore.instance.createRoom(otherUser!);
+                                                          kPushNavigator(context, FlyerDm(room: room,));
+                                                        },
+                                                        icon: Icon(
+                                                          Icons.send_rounded,
+                                                          color: Colors.grey[500],
+                                                          size: 20,
+                                                        )),
+                                                  ),
                                                 ),
-                                              ),
-                                            ))
+                                              ))
 
-                                    // const SizedBox(width: 10),
-                                    // const Spacer(),
-                                  ],
+                                      // const SizedBox(width: 10),
+                                      // const Spacer(),
+                                    ],
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
                       )
